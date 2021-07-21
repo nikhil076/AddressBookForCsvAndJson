c1d7b5da906208ad3b39bf54069013e28175b51e
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-21 20:33:24 +0530

    Added functionality to read/write the addressbook contacts as CSV file

--- lib/src/main/java/AddressBookForCSVAndJson/AddressBookMain.java
+++ lib/src/main/java/AddressBookForCSVAndJson/AddressBookMain.java
@@ -1,9 +1,17 @@
 package AddressBookForCSVAndJson;
 
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
 public class AddressBookMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CsvException {
 		System.out.println("Address book for CSV and Json");
+		
+		AddressBookCSV addressBookCSV = new AddressBookCSV();
+		addressBookCSV.writeContacatDetailsToFile();
+		addressBookCSV.readFromCSVFile();
+		
 	}
-
 }
